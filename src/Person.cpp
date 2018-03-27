1fa1bb43b2d639bfbcb747f7f1b2b85a319ad005
AnabelaSilva <up201506034@fe.up.pt>
2018-03-27 10:27:15 +0100

    Class Person

---
+++ src/Person.cpp
@@ -0,0 +1,18 @@
+/*
+ * Person.cpp
+ *
+ *  Created on: 27/03/2018
+ *      Author: anabela
+ */
+
+#include "Person.h"
+
+Person::Person() {
+	// TODO Auto-generated constructor stub
+
+}
+
+Person::~Person() {
+	// TODO Auto-generated destructor stub
+}
+
---
+++ src/Person.h
@@ -0,0 +1,17 @@
+/*
+ * Person.h
+ *
+ *  Created on: 27/03/2018
+ *      Author: anabela
+ */
+
+#ifndef SRC_PERSON_H_
+#define SRC_PERSON_H_
+
+class Person {
+public:
+	Person();
+	virtual ~Person();
+};
+
+#endif /* SRC_PERSON_H_ */
