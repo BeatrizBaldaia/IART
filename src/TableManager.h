cb8ee1a71c08cbd04afc2977ed21ded270740e62
BeatrizBaldaia <bibibaldaia@hotmail.com>
2018-03-27 10:16:49 +0100

    starting

---
+++ src/TableManager.h
@@ -0,0 +1,15 @@
+/*
+ * TableManager.h
+ *
+ *  Created on: 27/03/2018
+ *      Author: Utilizador
+ */
+
+#ifndef SRC_TABLEMANAGER_H_
+#define SRC_TABLEMANAGER_H_
+
+
+
+
+
+#endif /* SRC_TABLEMANAGER_H_ */
